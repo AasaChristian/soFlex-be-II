cc7a9c50ef710302103c601c0ac77d5b044bdff8
Aasa-Christian <aasachristian@yahoo.com>
2020-11-10 21:49:12 -0500

    updated cors obj

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ server.listen(Port, () => {
     console.log(`Listening at ${Port}`);
   });
   
-//   var corsOptions = {
-//       origin: process.env.BASE_URL || "http://localhost:3000",
-//       methods: "GET,PUT,POST,DELETE",
-//          credentials: false
-//   }
-//   app.use(cors(corsOptions));
+  var corsOptions = {
+      origin: process.env.BASE_URL || "http://localhost:3000",
+      methods: "GET,PUT,POST,DELETE",
+         credentials: false
+  }
+  app.use(cors(corsOptions));
   app.use(express.json());
 
   app.get("/", (req, res) => {
