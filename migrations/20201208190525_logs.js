d32a5e16932cfc1b7f4f661169fab3a8647d4282
Aasa-Christian <aasachristian@yahoo.com>
2020-12-08 19:15:25 -0500

    createed new migration tbl for user logs

---
+++ migrations/20201208190525_logs.js
@@ -0,0 +1,35 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable("logs", tbl => {
+        tbl.increments();
+        tbl.string('name').notNullable();
+        tbl.string('link')
+        tbl.integer("userId")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+
+        tbl.integer("exerciseId")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("exercises")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+
+        tbl.integer('sets')
+        tbl.integer('reps')
+        tbl.integer('weight')
+        tbl.boolean('completion')
+    })
+
+  
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("logs")
+  
+};
