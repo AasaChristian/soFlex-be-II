5d3312a059b501a6952f4c53aab6ae98e8f0d10e
Aasa-Christian <aasachristian@yahoo.com>
2021-05-03 20:11:49 -0400

    updated ssl

--- app.js
+++ app.js
@@ -12,24 +12,6 @@ const Logs = require('./Routers/logs-router')
 const cors = require('cors')
 
 
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.HEROKU_POSTGRESQL_IVORY_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
 
 
 server.listen(Port, () => {
