2ddf50d4fd598408be9906900e7889297eb45164
Aasa-Christian <aasachristian@yahoo.com>
2021-05-04 22:07:53 -0400

    updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -32,7 +32,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.HEROKU_POSTGRESQL_IVORY_URL,
+    connection: process.env.DATABASE_URL,
     
     pool: {
       min: 2,
