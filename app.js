80c19fb7ed6fbdf95f43e95d4f0ccdc663bf00aa
Aasa-Christian <aasachristian@yahoo.com>
2021-05-05 20:24:11 -0400

    updated ssl on knexfile

--- app.js
+++ app.js
@@ -10,44 +10,26 @@ const Exercies = require('./Routers/exercises-router')
 const Regimen = require('./Routers/regimen-router')
 const Logs = require('./Routers/logs-router')
 const cors = require('cors')
-const { Client } = require('pg');
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+server.listen(Port, () => {
+  console.log(`Listening at ${Port}`);
 });
 
-client.connect();
+var corsOptions = {
+    origin: process.env.BASE_URL || "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    methods: "GET,PUT,POST,DELETE",
+       credentials: false
+}
+app.use(cors(corsOptions));
+app.use(express.json());
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-
-  server.listen(Port, () => {
-    console.log(`Listening at ${Port}`);
-  });
-  
-  var corsOptions = {
-      origin: process.env.BASE_URL || "http://localhost:3000",
-      // origin: "http://localhost:3000",
-      methods: "GET,PUT,POST,DELETE",
-         credentials: false
-  }
-  app.use(cors(corsOptions));
-  app.use(express.json());
-
-  app.get("/", (req, res) => {
-    res.send({ response: "I am alive Test" }).status(200);
-  });
-
-  app.use('/api/users', Users);
-  app.use('/api/exercises', Exercies);
-  app.use('/api/regimen', Regimen);
-  app.use('/api/logs', Logs);
-  client.end();
+app.get("/", (req, res) => {
+  res.send({ response: "I am alive Test" }).status(200);
 });
+
+app.use('/api/users', Users);
+app.use('/api/exercises', Exercies);
+app.use('/api/regimen', Regimen);
+app.use('/api/logs', Logs);
 
