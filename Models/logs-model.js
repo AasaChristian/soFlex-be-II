ccc06b17a37753b701386388492a6e33ec8d1f78
Aasa-Christian <aasachristian@yahoo.com>
2021-05-29 19:32:25 -0400

    updated create new log model

--- Models/logs-model.js
+++ Models/logs-model.js
@@ -15,8 +15,9 @@ function findByuserId(userId){
     .select("regimen.id as regimenID", "regimen.weight as regimenWeight", "regimen.name as regimenName", "logs.weight as LoggedWeight", "logs.reps as LoggedReps","logs.set as LoggedSet","*" ) 
 }
 
-function addLog(log){
-    return db("logs").insert(log).returning("*")
+async function addLog(log){
+    const [userID] = await db("logs").insert(log).returning("userId")
+    return findByuserId(userID)
 }
 
 function findLoggedExercises(userId, exId){
